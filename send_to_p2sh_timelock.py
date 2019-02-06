363bb6ed6d147efe79718bfe4fb72d83eef91c15
Kim Rader <kimrader@gmail.com>
2019-02-06 13:34:11 -0800

    parameterized inputs
    Parameterized inputs as command-line arguments. More code clean-up, commenting.

--- send_to_p2sh_timelock.py
+++ send_to_p2sh_timelock.py
@@ -3,10 +3,12 @@
 # This script creates a P2SH address containing a timelock script.
 # All funds sent to the address will be locked for a period of time.
 #
-
 import sys
 if sys.version_info.major < 3:
     sys.stderr.write('Please use Python 3.x to run this.\n')
+    sys.exit(1)
+if len(sys.argv) <= 1:
+    sys.stderr.write('Usage: python3 send_to_p2sh_timelock.py [private key]\n')
     sys.exit(1)
 
 from bitcoin import SelectParams
@@ -18,12 +20,13 @@ OP_CHECKSEQUENCEVERIFY = OP_NOP3    #unlike other op-codes, python-bitcoinlib do
 
 SelectParams('testnet')  # CHANGE THIS TO regtest
 
-# todo: parameterize these
 # address we are spending from
-privKeySender = 'cVS929EhDuLUemssFz5N8BpuJZf6NpSdWmsENBBHUZyzMQLbYLmH'
+privKeySender = str(sys.argv[1])
 
 secret = CBitcoinSecret(privKeySender)
-#redeem_script = CScript([secret.pub, OP_CHECKSIG])
+
+# per instructor message in the COMP541-01-S-2019 Moodle classroom forum, use '0xc800' (200 blocks) for relative timelock amount
+# 0xc800 is calculated as little-endian hex of 200, plus '00' tacked on the end because it's necessary to add sign for values which are less than half a byte, otherwise interpreted as negative
 redeem_script = CScript([0xc800, OP_CHECKSEQUENCEVERIFY, OP_DROP, OP_DUP, OP_HASH160, Hash160(secret.pub), OP_EQUALVERIFY, OP_CHECKSIG])
 
 txin_scriptPubKey = redeem_script.to_p2sh_scriptPubKey()
--- spend_p2sh_timelock.py
+++ spend_p2sh_timelock.py
@@ -7,36 +7,43 @@ import sys
 if sys.version_info.major < 3:
     sys.stderr.write('Please use Python 3.x to run this.\n')
     sys.exit(1)
+if len(sys.argv) <= 2:
+    sys.stderr.write('Usage: python3 spend_p2sh_timelock.py [private key] [receiver address]\n')
+    sys.exit(1)    
 
 import bitcoin.rpc
+import binascii
 
 from bitcoin import SelectParams
-from bitcoin.core import b2x, lx, COIN, COutPoint, CMutableTxOut, CMutableTxIn, CMutableTransaction, Hash160
-from bitcoin.core.script import CScript, OP_DUP, OP_HASH160, OP_EQUALVERIFY, OP_CHECKSIG, SIGHASH_ALL, OP_DROP, OP_NOP3
+from bitcoin.core import b2x, b2lx, lx, COIN, COutPoint, CMutableTxOut, CMutableTxIn, CMutableTransaction, Hash160
+from bitcoin.core.script import CScript, CScriptOp, OP_DUP, OP_HASH160, SignatureHash, OP_EQUALVERIFY, OP_CHECKSIG, SIGHASH_ALL, OP_DROP, OP_NOP3
 from bitcoin.core.scripteval import VerifyScript, SCRIPT_VERIFY_P2SH
 from bitcoin.wallet import CBitcoinAddress, CBitcoinSecret
 
 OP_CHECKSEQUENCEVERIFY = OP_NOP3    #unlike other op-codes, python-bitcoinlib doesn't define OP_CHECKSEQUENCEVERIFY, so I define it myself here
 
-SelectParams('testnet')  # CHANGE THIS TO regtest
+SelectParams('testnet')
 
 # parameterize these
-privKeySender = 'cVS929EhDuLUemssFz5N8BpuJZf6NpSdWmsENBBHUZyzMQLbYLmH'
-addressReceiver = '2Myk6MB7DTEzuPC17v1voy5U8snwSULALyr'
+privKeySender = str(sys.argv[1])
+addressReceiver = str(sys.argv[2])
 
 secret = CBitcoinSecret(privKeySender)
-#redeem_script = CScript([secret.pub, OP_CHECKSIG])
+
+# per instructor message in the COMP541-01-S-2019 Moodle classroom forum, use '0xc800' (200 blocks) for relative timelock amount
+# 0xc800 is calculated as little-endian hex of 200, plus '00' tacked on the end because it's necessary to add sign for values which are less than half a byte, otherwise interpreted as negative
 redeem_script = CScript([0xc800, OP_CHECKSEQUENCEVERIFY, OP_DROP, OP_DUP, OP_HASH160, Hash160(secret.pub), OP_EQUALVERIFY, OP_CHECKSIG])
 
 txin_scriptPubKey = redeem_script.to_p2sh_scriptPubKey()
 txin_p2sh_address = CBitcoinAddress.from_scriptPubKey(txin_scriptPubKey)
-# print('Pay to:', str(txin_p2sh_address))
+print('Pay to:', str(txin_p2sh_address))
 
-# start intermission
 proxy = bitcoin.rpc.Proxy()
-#transaction = proxy.sendtoaddress(str(txin_p2sh_address), 0.0001*COIN)
-#print('transaction: ' + str(transaction))
-# end intermission
+
+# the below functionality is to be run from cmd line in between send_to_p2sh_timelock.py and this program
+# uncomment to run programmatically for testing purposes
+# transactionid = proxy.sendtoaddress(str(txin_p2sh_address), 0.0001*COIN)
+# print('transaction id: ' + b2lx(transactionid))
 
 # find the set of UTXO transactions that have been sent to the script address
 txins = []  # inputs to the spending transaction we will create
@@ -50,7 +57,7 @@ for transaction in proxy.call("listtransactions"):  # query the local node to fi
         # create a CMutableTxIn object so that we can test whether it's spendable
         txid = lx(transaction['txid'])  # lx() takes *little-endian* hex and converts it to bytes
         #txin = CMutableTxIn(COutPoint(txid, VOUT))
-        txin = CMutableTxIn(COutPoint(txid, VOUT), nSequence=0xc800007f)
+        txin = CMutableTxIn(COutPoint(txid, VOUT), nSequence=0xc800007f)    # per instructor msg in the forums, use '0xc800007f' for 200 blocks
         try:
             # test to see if this transaction is spendable
             # throws an IndexError error if it's not
@@ -66,18 +73,20 @@ for transaction in proxy.call("listtransactions"):  # query the local node to fi
             # multiply by -1 to make them positive
             # then add to total
             totalUnspentAmount += (transaction['amount'] * -1)
-assert (totalUnspentAmount > 0), 'Funds must be available to spend' # if script address has no UTXOs, throw an error and exit
+            #print('Including UTXO transaction ' + transaction['txid'] + ' for ' + str(transaction['amount'] * -1))
 
+# calculate the mining fee
 #FEE_PER_BYTE = 0.00025*COIN/1000    #todo: use estimatesmartfee function instead
 #mining_fee = len(txins.serialize()) * FEE_PER_BYTE
-mining_fee = 0.00000350
+mining_fee = 0.00000350     # todo: calculate this instead of hard-coding it
+assert(totalUnspentAmount > mining_fee), 'Not enough funds available to spend'
 output_amt = float(totalUnspentAmount) - mining_fee
 
 # Create the txout. This time we create the scriptPubKey from a Bitcoin address.
-txout = CMutableTxOut(output_amt*COIN, CBitcoinAddress(addressReceiver).to_scriptPubKey()) #todo: verify whether this is spendable
+txout = CMutableTxOut(output_amt*COIN, CBitcoinAddress(addressReceiver).to_scriptPubKey())
 
 # Create the unsigned transaction.
-tx = CMutableTransaction(txins, [txout])
+tx = CMutableTransaction(txins, [txout])  # maybe need to add parameter 'nLockTime=200'? It seems to work without this parameter, but slide 74 says it's needed
 #print("raw unsigned transaction: " + b2x(tx.serialize()))
 
 for index, txin in enumerate(txins):
@@ -98,11 +107,13 @@ for index, txin in enumerate(txins):
     # Verify the signature worked. This calls EvalScript() and actually executes
     # the opcodes in the scripts to see if everything worked out. If it doesn't an
     # exception will be raised.
-    VerifyScript(txin.scriptSig, txin_scriptPubKey, tx, index, (SCRIPT_VERIFY_P2SH,))
+    # this is commented out so that locktime requirement error is not hidden when this script is run before locktime requirement is satisfied
+    # VerifyScript(txin.scriptSig, txin_scriptPubKey, tx, index, (SCRIPT_VERIFY_P2SH,))
 
-# Done! Print the transaction to standard output with the bytes-to-hex
-# function.
-print('transaction: ' + b2x(tx.serialize()))
+# Done setting up the transaction. Display it.
+# print('transaction: ' + b2x(tx.serialize()))
 
-#actually execute the transaction
-#proxy.sendrawtransaction(tx)
+# actually execute the transaction
+transactionid = proxy.sendrawtransaction(tx)
+# decode and print the resulting transaction id. Using bytes to little-endian hex function.
+print('transaction id: ' + b2lx(transactionid))
